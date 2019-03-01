1a715060431789e5969efa9b176049d2d7f8c845
Andrij Kulynych <akulmmm@gmail.com>
2019-03-02 01:09:33 +0200

    delete paket homework3

--- src/com/mateakademia/homework/homework3/task32/lamborghinicar/Main.java
+++
@@ -1,9 +0,0 @@
-package com.mateakademia.homework.homework3.task32.lamborghinicar;
-
-
-public class Main {
-    public static void main (String[] args) {
-        Car car1 = new Car();
-        car1.showInfo();
-    }
-}
