e8a7dd7ff117b2b3d2bc91cfdad477f937b2706d
Andrij Kulynych <akulmmm@gmail.com>
2019-03-02 09:47:03 +0200

    Add Car and anothter *.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.mateakademia.homework.homework3.task32.lamborghinicar;
+
+
+public class Main {
+    public static void main (String[] args) {
+        Car car1 = new Car();
+        car1.showInfo();
+    }
+}
