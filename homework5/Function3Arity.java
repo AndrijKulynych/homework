c08b66945c6bb4c687ae7e7f88f53c7681b23308
Andrij Kulynych <akulmmm@gmail.com>
2019-03-10 18:49:35 +0200

    Add homework5

---
+++ homework5/Function3Arity.java
@@ -0,0 +1,6 @@
+package com.jv_program.homework5;
+
+public interface Function3Arity <  A, B, C, R >{
+    R apply(Double a, String b, Double c);
+
+}
---
+++ homework5/Printer.java
@@ -0,0 +1,5 @@
+package com.jv_program.homework5;
+
+public class Printer {
+void pritnln (String str){}
+}
